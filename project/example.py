50c56202ab0241fb951cc4df1157bdf53a60f490
borisovaldv <borisovaldv@gmail.com>
2022-11-24 23:10:26 +0300

    Learn docstring testing

--- project/example.py
+++ project/example.py
@@ -1,2 +1,16 @@
 def reverse(string):
+    """Reverse string
+
+    >>> reverse('')
+    ''
+
+    >>> reverse('Hexlet')
+    'telxeH'
+    """
+
     return string[::-1]
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
